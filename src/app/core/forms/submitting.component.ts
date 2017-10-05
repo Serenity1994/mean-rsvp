d8647553419a0cb14a8541092249ae8d96f6742a
Serenity1994 <846288113@qq.com>
2017-10-05 23:19:13 +0800

    https://auth0.com/blog/real-world-angular-series-part-6/

---
+++ src/app/core/forms/submitting.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-submitting',
+  template: `
+    <img src="../../../assets/images/loading.svg"/>
+  `,
+  styles: [`
+    :host {
+      display: inline-block;
+    }
+    img {
+      display: inline-block;
+      margin: 4px 3px;
+      width: 30px;
+    }
+  `]
+})
+export class SubmittingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
